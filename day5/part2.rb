ff42ddf941375947e41bc93a1781fd5ca113e872
AnotherPillow <Thepilous_@outlook.com>
2022-12-05 20:43:14 +0000

    day 5
    that was hard

---
+++ day5/part2.rb
@@ -0,0 +1,21 @@
+file = File.read('input.txt')
+stacks = file.split("\n\n")[0]
+instructions = file.split("\n\n")[1]
+stacks = stacks.split("\n").map(&:chars).reverse.transpose
+
+stacks = stacks.select{|first, *rest| first.match?(/\d+/) }
+
+stacks.map{|list| list.delete(" ")}
+
+stacks = stacks.map{|first, *rest| [first, rest]}.to_h
+
+
+instructions.split("\n").each do |instruction|
+  move, source, dest = instruction.scan(/\d+/)
+  
+  crate = stacks[source].pop(move.to_i)
+
+  stacks[dest] += crate
+end
+
+puts stacks.values.map(&:last).join
